2035948a69b230236070f1d0f4d33f5ea29f7cde
Aptheparker <aptheparker@gmail.com>
2022-07-19 15:54:30 +0900

    login practice

---
+++ app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//module
+const express = require('express');
+const app = express();
+
+//routing
+const home = require('./src/routes/home');
+
+//app setting
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+//middle
+app.use('/', home);
+
+module.exports = app;
