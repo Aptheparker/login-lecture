6b7d702dd32742fef0128ec573661bf8be2e6d99
Aptheparker <aptheparker@gmail.com>
2022-07-21 12:28:10 +0900

    using dom

---
+++ app/src/public/js/home/login.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const id = document.querySelector('#id'),
+  password = document.querySelector('#password'),
+  loginButton = document.querySelector('button');
+
+loginButton.addEventListener('click', login);
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
